0d7c1a4d0421bc221a4675ff2e5d91f3085f2df1
Drtopetmi <120068210+Drtopetmi@users.noreply.github.com>
2022-12-23 05:46:39 +0200

    Updated app.js

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   let apiKey = "63214c4281922e3bb72fdf12dada7734";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
+  axios.get(apiUrl).then(displayForecast);}
 
 function showTemperature(response) {
  let cityElement = document.querySelector("#city");
